21da530c72ac253b1e650ffa0a0b9d0206a65857
Kaique Merlo <kaique.merlo@hotmail.com>
2015-03-12 22:00:32 -0300

    Criando classe e fazendo acao para exibir detalhes do cliente

---
+++ classes/Pessoa.class.php
@@ -0,0 +1,44 @@
+<?php 
+class Pessoa{
+	// Atributos da classe
+	static $indice;
+	public $nome;
+	public $cpf;
+	public $rg;
+	public $peso;
+	public $idade;
+	public $endereco;
+
+
+	// Método construtor, ele é carregado quando a classe é instanciada
+	public function __construct($nome, $cpf, $rg=null, $peso=null, $idade=null, $endereco=null){
+		$this->nome     = $nome;
+		$this->cpf      = $cpf;
+		$this->rg       = $rg;
+		$this->peso     = $peso;
+		$this->idade    = $idade;
+		$this->endereco = $endereco;
+	}
+
+	// Método responsavel por listar os clientes e seus respectivos links
+	public function listaClientes(){
+		self::$indice ++;
+		$i = self::$indice - 1;
+		echo "<a href='index.php?indice=$i'>$this->nome</a><br>";
+	}
+
+	// Método responsavel por listar os dados gerais dos clientes
+	public function dadosCliente(){
+		echo $this->nome;
+		echo $this->cpf;
+		echo $this->rg;
+		echo $this->peso;
+		echo $this->idade;
+		echo $this->endereco;
+	}
+
+}
+
+
+
+ ?>
